948ecb8d21ec57874f3c8d32b1aa89bc98a6e0a2
Mason Li <masli@chapman.edu>
2025-05-22 15:32:02 -0700

    Corrected SwiftData declaration in SimpleBudgetApp.

--- SimpleBudget/App/SimpleBudgetApp.swift
+++ SimpleBudget/App/SimpleBudgetApp.swift
@@ -11,11 +11,24 @@ import SwiftData
 @main
 struct SimpleBudgetApp: App {
     // SwiftData container for Expense model
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Expense.self,
+            User.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: Expense.self)
+                .modelContainer(sharedModelContainer)
         }
     }
 }
