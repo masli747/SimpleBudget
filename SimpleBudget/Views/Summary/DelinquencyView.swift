4bd0155bc9a2503a4f2181820bf004cc2ab4ddc4
Mason Li <masli@chapman.edu>
2025-05-22 16:31:45 -0700

    Implemented creation and adding of user class through application.

--- SimpleBudget/Views/Summary/DelinquencyView.swift
+++ SimpleBudget/Views/Summary/DelinquencyView.swift
@@ -6,10 +6,22 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct DelinquencyView: View {
+    @Environment(\.modelContext) private var context
+    @Query var user: [User]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let user = user.first {
+            Form {
+                Section(header: Text("User")) {
+                    TextField("Name", text: .constant(user.name))
+                }
+            }
+        } else {
+            Text("User Not Configured...")
+        }
     }
 }
 
