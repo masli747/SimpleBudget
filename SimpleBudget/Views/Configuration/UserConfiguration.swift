411147990645197d6fa2329367361c4aa3ec7410
Mason Li <masli@chapman.edu>
2025-05-22 15:05:40 -0700

    Basic prototype for creation of a user object through the UserConfiguration view.

--- SimpleBudget/Views/Configuration/UserConfiguration.swift
+++ SimpleBudget/Views/Configuration/UserConfiguration.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct UserConfiguration: View {
+    @Environment(\.modelContext) private var context
+    @Query var user: [User]
+    @State private var name: String = ""
+    @State private var age: Int = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let user = user.first {
+            Form {
+                Section(header: Text("User")) {
+                    TextField("Name", text: .constant(user.name))
+                }
+            }
+        } else {
+            Form {
+                Section(header: Text("User Information")) {
+                    TextField("Name", text: $name)
+                    VStack {
+                        Text("Select Age:")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Picker("Select Age", selection: $age) {
+                            ForEach(1..<101, id: \.self) { age in
+                                Text("\(age)").tag(age)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(height: 125)
+                    }
+                }
+                Section(header: Text("Financial Information")) {
+                    
+                }
+            }
+        }
+        //            .navigationTitle("Configure User")
     }
 }
 
