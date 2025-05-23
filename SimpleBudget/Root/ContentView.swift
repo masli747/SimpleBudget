108a75a2cc4103170dc6e7e689b1bdb6bd567af7
Mason Li <masli@chapman.edu>
2025-05-22 23:07:30 -0700

    Attempting to apply lifecycle refreshing for Delinquency predictipon page.

--- SimpleBudget/Root/ContentView.swift
+++ SimpleBudget/Root/ContentView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @State private var selection: Tab = .root
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
             DashboardView()
                 .tabItem { Label("Dashboard",  systemImage: "house") }
+                .tag(Tab.root)
             AddExpenseView()
                 .tabItem { Label("Log",        systemImage: "plus.circle") }
+                .tag(Tab.expense)
             WeeklyView()
                 .tabItem { Label("Weekly",     systemImage: "chart.pie") }
+                .tag(Tab.weekly)
             MonthlyView()
                 .tabItem { Label("Monthly",    systemImage: "chart.bar") }
-            DelinquencyView()
+                .tag(Tab.monthly)
+            DelinquencyView(selectedTab: $selection)
                 .tabItem { Label("Delinquency", systemImage: "exclamationmark.triangle") }
+                .tag(Tab.delinquency)
             SettingsView()
                 .tabItem { Label("Settings",   systemImage: "gear") }
+                .tag(Tab.settings)
         }
     }
 }
 
+enum Tab {
+  case root, dashboard, expense, weekly, monthly, delinquency, settings
+}
 
 #Preview {
     ContentView()
--- SimpleBudget/Views/Summary/DelinquencyView.swift
+++ SimpleBudget/Views/Summary/DelinquencyView.swift
@@ -9,22 +9,32 @@ import SwiftUI
 import SwiftData
 
 struct DelinquencyView: View {
+    @Binding var selectedTab: Tab
     @Environment(\.modelContext) private var context
+    @Environment(\.refresh) private var refresh
     @Query var user: [User]
     
+    
     var body: some View {
-        if let user = user.first {
-            Form {
-                Section(header: Text("User")) {
-                    TextField("Name", text: .constant(user.name))
+        Group {
+            if let user = user.first {
+                Form {
+                    Section(header: Text("User")) {
+                        TextField("Name", text: .constant(user.name))
+                    }
                 }
+            } else {
+                Text("User Not Configured...")
             }
-        } else {
-            Text("User Not Configured...")
+        }
+        .onChange(of: selectedTab) {
+            Task {
+                await refresh?()
+            }
         }
     }
 }
 
 #Preview {
-    DelinquencyView()
+    DelinquencyView(selectedTab: .constant(.delinquency))
 }
